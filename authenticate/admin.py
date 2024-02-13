9971b40bef0862572f736d479f71412163d05b2b
Jonathan Ochoa <ochoajonathan@outlook.com>
2024-02-12 21:00:12 -0500

    added password status to users page in admin console.

--- authenticate/admin.py
+++ authenticate/admin.py
@@ -5,6 +5,7 @@ from .models import CustomUser
 from django.shortcuts import redirect
 from django.utils.html import format_html
 from django.urls import reverse
+from django.utils import timezone
 
 
 class CustomUserAdmin(UserAdmin):
@@ -25,12 +26,20 @@ class CustomUserAdmin(UserAdmin):
         (_('Security Questions'), {'fields': ('question1', 'answer1', 'question2', 'answer2')}),
         (_('Suspension Info'), {'fields': ('suspension_start_date', 'suspension_end_date')}),
     )
-    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'is_suspended', 'suspension_start_date', 'suspension_end_date', 'dob', 'status', 'send_email_link')
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'is_suspended', 'suspension_start_date', 'suspension_end_date', 'dob', 'status', 'send_email_link', 'password_expiry_status')
     search_fields = ('username', 'first_name', 'last_name', 'email')
 
     def send_email_link(self, obj):
         return format_html('<a href="{}">Send Email</a>', reverse('send_email', args=[obj.pk]))
     send_email_link.short_description = 'Send Email'
+
+    def password_expiry_status(self, obj):
+        if obj.password_expiry and obj.password_expiry <= timezone.now().date():
+            return "Expired"
+        else:
+            return "Valid"
+    
+    password_expiry_status.short_description = 'Password Status'
 
     actions = None
 
