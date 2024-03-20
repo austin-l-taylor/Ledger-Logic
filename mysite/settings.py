4a2150eb278bb27f2e3f2192f10181463d7d4add
Jonathan Ochoa <ochoajonathan@outlook.com>
2024-03-19 21:28:15 -0400

    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')

--- mysite/settings.py
+++ mysite/settings.py
@@ -5,6 +5,7 @@ from dotenv import load_dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
