3b01460f73fbbaf2f664897c12ddbea1e762ce00
Jonathan Ochoa <ochoajonathan@outlook.com>
2024-03-30 17:10:39 -0400

    added attachment column to journalentry model and added media folder to root

--- authenticate/models.py
+++ authenticate/models.py
@@ -196,6 +196,8 @@ class JournalEntry(models.Model):
     credit = models.DecimalField(max_digits=10, decimal_places=2, default=0)
     status = models.CharField(max_length=100, choices=STATUS_CHOICES, default='Pending')
     comments = models.TextField(blank=True, null=True)
+    #attachement column, this will automatically add the files to a sub-directory called journal_entries inside the media file in the root
+    attachment = models.FileField(upload_to='journal_entries/', null=True, blank=True)
     
     def approve(self):
         if self.status == 'Pending':
